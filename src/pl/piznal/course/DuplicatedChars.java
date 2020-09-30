3fd5f52bcc489fe08b2593e1c0440271122a9c88
Your Name <you@example.com>
2020-09-30 11:27:43 +0200

    Duplicated chars

---
+++ src/pl/piznal/course/DuplicatedChars.java
@@ -0,0 +1,58 @@
+package pl.piznal.course;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toSet;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+public class DuplicatedChars {
+  public static void main(String[] args) {
+    int attempts = 10;
+    long start, finish;
+
+    System.out.println("Starting stream version for Jaskolka");
+    start = System.currentTimeMillis();
+    IntStream.range(1, attempts).forEach(i -> notDuplicatedCharacters.test("Jaskolka"));
+    finish = System.currentTimeMillis();
+    System.out.println(format("It took %d miliseconds for %d probes", finish - start, attempts));
+
+    System.out.println("Starting algo version for Jaskolka");
+    start = System.currentTimeMillis();
+    IntStream.range(1, attempts).forEach(i -> notDuplicatedCharactesAlgo("Jaskolka"));
+    finish = System.currentTimeMillis();
+    System.out.println(format("It took %d miliseconds for %d probes", finish - start, attempts));
+
+    System.out.println("Starting stream version for Wilk");
+    start = System.currentTimeMillis();
+    IntStream.range(1, attempts).forEach(i -> notDuplicatedCharacters.test("Wilk"));
+    finish = System.currentTimeMillis();
+    System.out.println(format("It took %d miliseconds for %d probes", finish - start, attempts));
+
+
+    System.out.println("Starting algo version for Wilk");
+    start = System.currentTimeMillis();
+    IntStream.range(1, attempts).forEach(i -> notDuplicatedCharactesAlgo("Wilk"));
+    finish = System.currentTimeMillis();
+    System.out.println(format("It took %d miliseconds for %d probes", finish - start, attempts));
+  }
+
+
+  static Predicate<String> notDuplicatedCharacters =
+      s -> s.chars().boxed().collect(toSet()).size() == s.length();
+
+  static boolean notDuplicatedCharactesAlgo(String str) {
+    char[] chars = str.toCharArray();
+    char[] temp = new char[str.length()];
+
+    for (int i = 0; i < chars.length; i++) {
+      for (int j = 0; j < temp.length; j++) {
+        if (temp[j] == chars[i]) {
+          return false;
+        }
+      }
+      temp[i] = chars[i];
+    }
+
+    return true;
+  }
+}
